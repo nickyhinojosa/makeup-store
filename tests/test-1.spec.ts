92d09f29aea4bc7b593c1c773a7f6b0f2374cf5b
Jilmar <jilmar141@gmail.com>
2025-05-29 23:48:40 -0400

    add integration test and sistem test

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from "@playwright/test";
 
-test("Iniciar sesion en el sistema", async ({ page }) => {
+test("Prueba de integracion", async ({ page }) => {
   await page.goto("http://localhost:3000/login");
-  await page.locator(".relative > .inline-flex").first().click();
+  await page.getByRole("textbox", { name: "Usuario Usuario" }).click();
   await page
     .getByRole("textbox", { name: "Usuario Usuario" })
     .fill("superadmin");
@@ -11,4 +11,65 @@ test("Iniciar sesion en el sistema", async ({ page }) => {
     .getByRole("textbox", { name: "Contraseña Contraseña" })
     .fill("12345");
   await page.getByRole("button", { name: "Iniciar" }).click();
+  await page.getByRole("link", { name: "Categorias" }).click();
+  await page.getByRole("button", { name: "Nueva categoria" }).click();
+  await page.getByRole("textbox", { name: "Nombre categoria Nombre" }).click();
+  await page
+    .getByRole("textbox", { name: "Nombre categoria Nombre" })
+    .fill("Mascarillas");
+  await page
+    .locator("div")
+    .filter({ hasText: /^Descripcion$/ })
+    .nth(1)
+    .click();
+  await page
+    .getByRole("textbox", { name: "Descripcion Descripcion" })
+    .fill("Productos solo para el uso en el rostro.");
+  await page.getByRole("button", { name: "Crear categoria" }).click();
+  await page.getByRole("link", { name: "Productos" }).click();
+  await page.getByRole("button", { name: "Nuevo producto" }).click();
+  await page.getByRole("textbox", { name: "Nombre producto Nombre" }).click();
+  await page
+    .getByRole("textbox", { name: "Nombre producto Nombre" })
+    .fill("Kerastase");
+  await page.getByRole("textbox", { name: "Descripción Descripción" }).click();
+  await page
+    .getByRole("textbox", { name: "Descripción Descripción" })
+    .fill("Resistance masque force architecte");
+  await page.locator("div:nth-child(4) > .relative > .inline-flex").click();
+  await page
+    .getByRole("textbox", { name: "Imagen producto Imagen" })
+    .fill(
+      "https://www.kerastase-centroamerica.com/cuidado-para-tu-cabello/-/media/Project/Loreal/Brand%20Sites/Kerastase/AMERICAS/LATAM/Products/Blog%20products/KER_RESISTANCE.jpg"
+    );
+  await page.locator("div:nth-child(5) > .relative > .inline-flex").click();
+  await page
+    .getByRole("spinbutton", { name: "Precio producto Precio" })
+    .fill("250");
+  await page.locator("div:nth-child(6) > .relative > .inline-flex").click();
+  await page
+    .getByRole("spinbutton", { name: "Stock producto Stock producto" })
+    .fill("50");
+  await page
+    .getByRole("textbox", { name: "Fecha de caducidad Fecha de" })
+    .fill("2025-12-18");
+  await page.getByRole("button", { name: "Categoría" }).click();
+  await page
+    .getByLabel("Mascarillas", { exact: true })
+    .getByText("Mascarillas")
+    .click();
+  await page.getByRole("button", { name: "Crear producto" }).click();
+  await page
+    .getByRole("row", { name: "Kerastase Kerastase" })
+    .locator("svg")
+    .nth(1)
+    .click();
+  await page.getByRole("button", { name: "Borrar producto" }).click();
+  await page.getByRole("link", { name: "Categorias" }).click();
+  await page
+    .getByRole("row", { name: "Mascarillas" })
+    .locator("svg")
+    .nth(1)
+    .click();
+  await page.getByRole("button", { name: "Borrar categoria" }).click();
 });
--- tests/test-2.spec.ts
+++ tests/test-2.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from "@playwright/test";
 
-test("Crear categoria", async ({ page }) => {
+test("Prueba de sistema", async ({ page }) => {
   await page.goto("http://localhost:3000/login");
-  await page.locator(".relative > .inline-flex").first().click();
+  await page.getByRole("textbox", { name: "Usuario Usuario" }).click();
   await page
     .getByRole("textbox", { name: "Usuario Usuario" })
     .fill("superadmin");
@@ -11,15 +11,99 @@ test("Crear categoria", async ({ page }) => {
     .getByRole("textbox", { name: "Contraseña Contraseña" })
     .fill("12345");
   await page.getByRole("button", { name: "Iniciar" }).click();
-  await page.getByRole("link", { name: "Categorias" }).click();
-  await page.getByRole("button", { name: "Nueva categoria" }).click();
-  await page.locator(".relative > .inline-flex").first().click();
+  await page.getByRole("link", { name: "Realizar venta" }).click();
+  await page.getByRole("button", { name: "labios" }).click();
+  await page.locator("div:nth-child(2) > .pt-2 > div > .z-0").click();
+  await page.getByRole("button", { name: "ojos" }).click();
+  await page.locator("div:nth-child(2) > .pt-2 > div > .z-0").click();
+  await page.getByRole("button", { name: "rostro" }).click();
+  await page.locator("div:nth-child(2) > .pt-2 > div > .z-0").click();
+  await page.getByRole("button", { name: "Skin-Care" }).click();
+  await page.getByRole("button", { name: "Agregar" }).first().click();
+  await page.getByRole("button", { name: "Brochas" }).click();
+  await page.getByRole("button", { name: "Agregar" }).nth(1).click();
   await page
-    .getByRole("textbox", { name: "Nombre categoria Nombre" })
-    .fill("Mascarillas");
-  await page.getByRole("textbox", { name: "Descripcion Descripcion" }).click();
+    .locator("div")
+    .filter({ hasText: /^55 Bs\.$/ })
+    .getByRole("img")
+    .nth(1)
+    .click();
   await page
-    .getByRole("textbox", { name: "Descripcion Descripcion" })
-    .fill("Productos para el rostro.");
-  await page.getByRole("button", { name: "Crear categoria" }).click();
+    .locator(
+      "div:nth-child(2) > div:nth-child(2) > div > div > .relative > .inline-flex > span:nth-child(3) > svg > path"
+    )
+    .click();
+  await page
+    .locator(
+      "div:nth-child(2) > div:nth-child(2) > div > div > .relative > .inline-flex > span:nth-child(3) > svg > path"
+    )
+    .click();
+  await page
+    .locator(
+      "div:nth-child(3) > div:nth-child(2) > div > div > .relative > .inline-flex > span:nth-child(3) > svg"
+    )
+    .click();
+  await page
+    .locator("div")
+    .filter({ hasText: /^130 Bs\.$/ })
+    .getByRole("img")
+    .nth(1)
+    .click();
+  await page
+    .locator("div")
+    .filter({ hasText: /^195 Bs\.$/ })
+    .getByRole("img")
+    .nth(1)
+    .click();
+  await page
+    .locator("div")
+    .filter({ hasText: /^65 Bs\.$/ })
+    .getByRole("img")
+    .nth(1)
+    .click();
+  await page
+    .locator(
+      "div:nth-child(4) > div:nth-child(2) > div > div > .relative > .inline-flex > span:nth-child(3) > svg > path"
+    )
+    .click();
+  await page
+    .locator(
+      "div:nth-child(4) > div:nth-child(2) > div > div > .relative > .inline-flex > span:nth-child(3) > svg > path"
+    )
+    .click();
+  await page
+    .locator(
+      "div:nth-child(4) > div:nth-child(2) > div > div > .relative > .inline-flex > span:nth-child(3) > svg"
+    )
+    .click();
+  await page
+    .locator(
+      "div:nth-child(5) > div:nth-child(2) > div > div > .relative > .inline-flex > span:nth-child(3) > svg > path"
+    )
+    .click();
+  await page
+    .locator(
+      "div:nth-child(5) > div:nth-child(2) > div > div > .relative > .inline-flex > span:nth-child(3) > svg > path"
+    )
+    .click();
+  await page
+    .locator(
+      "div:nth-child(5) > div:nth-child(2) > div > div > .relative > .inline-flex > span:nth-child(3) > svg > path"
+    )
+    .click();
+  await page
+    .locator(
+      "div:nth-child(5) > div:nth-child(2) > div > div > .relative > .inline-flex > span:nth-child(3) > svg > path"
+    )
+    .click();
+  await page.getByRole("button", { name: "Show suggestions" }).click();
+  await page.getByText("Faviola Rodriguez -").click();
+  await page
+    .getByRole("button", { name: "sin descuento - 0% , Descuento" })
+    .click();
+  await page
+    .getByLabel("Dia de las madres - 25%", { exact: true })
+    .getByText("Dia de las madres - 25%")
+    .click();
+  await page.getByRole("button", { name: "Vender" }).click();
 });

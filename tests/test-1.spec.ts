a40b778fa7863c450e90d688f21e14ed232eae2a
Jilmar <jilmar141@gmail.com>
2025-05-30 12:13:49 -0400

    unit test and sistem test

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,12 +1,12 @@
 import { test, expect } from "@playwright/test";
 
-test("Prueba de integracion", async ({ page }) => {
+test("Prueba de integracion categorias con productos", async ({ page }) => {
   await page.goto("http://localhost:3000/login");
   await page.getByRole("textbox", { name: "Usuario Usuario" }).click();
   await page
     .getByRole("textbox", { name: "Usuario Usuario" })
     .fill("superadmin");
-  await page.getByRole("textbox", { name: "Usuario Usuario" }).press("Tab");
+  await page.getByRole("textbox", { name: "Contraseña Contraseña" }).click();
   await page
     .getByRole("textbox", { name: "Contraseña Contraseña" })
     .fill("12345");
@@ -17,14 +17,10 @@ test("Prueba de integracion", async ({ page }) => {
   await page
     .getByRole("textbox", { name: "Nombre categoria Nombre" })
     .fill("Mascarillas");
-  await page
-    .locator("div")
-    .filter({ hasText: /^Descripcion$/ })
-    .nth(1)
-    .click();
+  await page.getByRole("textbox", { name: "Descripcion Descripcion" }).click();
   await page
     .getByRole("textbox", { name: "Descripcion Descripcion" })
-    .fill("Productos solo para el uso en el rostro.");
+    .fill("Productos unicamente para el uso en el rostro.");
   await page.getByRole("button", { name: "Crear categoria" }).click();
   await page.getByRole("link", { name: "Productos" }).click();
   await page.getByRole("button", { name: "Nuevo producto" }).click();
@@ -35,8 +31,8 @@ test("Prueba de integracion", async ({ page }) => {
   await page.getByRole("textbox", { name: "Descripción Descripción" }).click();
   await page
     .getByRole("textbox", { name: "Descripción Descripción" })
-    .fill("Resistance masque force architecte");
-  await page.locator("div:nth-child(4) > .relative > .inline-flex").click();
+    .fill("Unicamente para el uso en el rostro.");
+  await page.getByRole("textbox", { name: "Imagen producto Imagen" }).click();
   await page
     .getByRole("textbox", { name: "Imagen producto Imagen" })
     .fill(
@@ -46,7 +42,9 @@ test("Prueba de integracion", async ({ page }) => {
   await page
     .getByRole("spinbutton", { name: "Precio producto Precio" })
     .fill("250");
-  await page.locator("div:nth-child(6) > .relative > .inline-flex").click();
+  await page
+    .getByRole("spinbutton", { name: "Stock producto Stock producto" })
+    .click();
   await page
     .getByRole("spinbutton", { name: "Stock producto Stock producto" })
     .fill("50");
@@ -59,17 +57,4 @@ test("Prueba de integracion", async ({ page }) => {
     .getByText("Mascarillas")
     .click();
   await page.getByRole("button", { name: "Crear producto" }).click();
-  await page
-    .getByRole("row", { name: "Kerastase Kerastase" })
-    .locator("svg")
-    .nth(1)
-    .click();
-  await page.getByRole("button", { name: "Borrar producto" }).click();
-  await page.getByRole("link", { name: "Categorias" }).click();
-  await page
-    .getByRole("row", { name: "Mascarillas" })
-    .locator("svg")
-    .nth(1)
-    .click();
-  await page.getByRole("button", { name: "Borrar categoria" }).click();
 });
--- tests/test-2.spec.ts
+++ tests/test-2.spec.ts
@@ -1,109 +1,28 @@
 import { test, expect } from "@playwright/test";
 
-test("Prueba de sistema", async ({ page }) => {
+test("Prueba de sistema realizar una venta", async ({ page }) => {
   await page.goto("http://localhost:3000/login");
-  await page.getByRole("textbox", { name: "Usuario Usuario" }).click();
-  await page
-    .getByRole("textbox", { name: "Usuario Usuario" })
-    .fill("superadmin");
+  await page.locator(".relative > .inline-flex").first().click();
+  await page.getByRole("textbox", { name: "Usuario Usuario" }).fill("nicol");
   await page.getByRole("textbox", { name: "Usuario Usuario" }).press("Tab");
   await page
     .getByRole("textbox", { name: "Contraseña Contraseña" })
     .fill("12345");
   await page.getByRole("button", { name: "Iniciar" }).click();
   await page.getByRole("link", { name: "Realizar venta" }).click();
-  await page.getByRole("button", { name: "labios" }).click();
-  await page.locator("div:nth-child(2) > .pt-2 > div > .z-0").click();
   await page.getByRole("button", { name: "ojos" }).click();
-  await page.locator("div:nth-child(2) > .pt-2 > div > .z-0").click();
+  await page.getByRole("button", { name: "Agregar" }).click();
+  await page.getByRole("button", { name: "Agregar" }).click();
   await page.getByRole("button", { name: "rostro" }).click();
-  await page.locator("div:nth-child(2) > .pt-2 > div > .z-0").click();
-  await page.getByRole("button", { name: "Skin-Care" }).click();
-  await page.getByRole("button", { name: "Agregar" }).first().click();
-  await page.getByRole("button", { name: "Brochas" }).click();
-  await page.getByRole("button", { name: "Agregar" }).nth(1).click();
-  await page
-    .locator("div")
-    .filter({ hasText: /^55 Bs\.$/ })
-    .getByRole("img")
-    .nth(1)
-    .click();
-  await page
-    .locator(
-      "div:nth-child(2) > div:nth-child(2) > div > div > .relative > .inline-flex > span:nth-child(3) > svg > path"
-    )
-    .click();
-  await page
-    .locator(
-      "div:nth-child(2) > div:nth-child(2) > div > div > .relative > .inline-flex > span:nth-child(3) > svg > path"
-    )
-    .click();
-  await page
-    .locator(
-      "div:nth-child(3) > div:nth-child(2) > div > div > .relative > .inline-flex > span:nth-child(3) > svg"
-    )
-    .click();
-  await page
-    .locator("div")
-    .filter({ hasText: /^130 Bs\.$/ })
-    .getByRole("img")
-    .nth(1)
-    .click();
-  await page
-    .locator("div")
-    .filter({ hasText: /^195 Bs\.$/ })
-    .getByRole("img")
-    .nth(1)
-    .click();
-  await page
-    .locator("div")
-    .filter({ hasText: /^65 Bs\.$/ })
-    .getByRole("img")
-    .nth(1)
-    .click();
-  await page
-    .locator(
-      "div:nth-child(4) > div:nth-child(2) > div > div > .relative > .inline-flex > span:nth-child(3) > svg > path"
-    )
-    .click();
-  await page
-    .locator(
-      "div:nth-child(4) > div:nth-child(2) > div > div > .relative > .inline-flex > span:nth-child(3) > svg > path"
-    )
-    .click();
-  await page
-    .locator(
-      "div:nth-child(4) > div:nth-child(2) > div > div > .relative > .inline-flex > span:nth-child(3) > svg"
-    )
-    .click();
-  await page
-    .locator(
-      "div:nth-child(5) > div:nth-child(2) > div > div > .relative > .inline-flex > span:nth-child(3) > svg > path"
-    )
-    .click();
-  await page
-    .locator(
-      "div:nth-child(5) > div:nth-child(2) > div > div > .relative > .inline-flex > span:nth-child(3) > svg > path"
-    )
-    .click();
-  await page
-    .locator(
-      "div:nth-child(5) > div:nth-child(2) > div > div > .relative > .inline-flex > span:nth-child(3) > svg > path"
-    )
-    .click();
-  await page
-    .locator(
-      "div:nth-child(5) > div:nth-child(2) > div > div > .relative > .inline-flex > span:nth-child(3) > svg > path"
-    )
-    .click();
+  await page.getByRole("button", { name: "Agregar" }).click();
   await page.getByRole("button", { name: "Show suggestions" }).click();
-  await page.getByText("Faviola Rodriguez -").click();
+  await page.getByText("Julia Roberts -").click();
   await page
     .getByRole("button", { name: "sin descuento - 0% , Descuento" })
     .click();
   await page
-    .getByLabel("Dia de las madres - 25%", { exact: true })
-    .getByText("Dia de las madres - 25%")
+    .getByLabel("Cumpleaños - 20%", { exact: true })
+    .getByText("Cumpleaños - 20%")
     .click();
   await page.getByRole("button", { name: "Vender" }).click();
 });
